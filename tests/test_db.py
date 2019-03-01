1ade03cb88c30995c81f4896fa20569f5beb24b5
Hunter Parks <hunter.thomas.parks@gmail.com>
2019-03-01 14:31:20 +0900

    pass tests

--- tests/test_db.py
+++ tests/test_db.py
@@ -19,8 +19,8 @@ def test_init_db_command(runner, monkeypatch):
 
     def fake_init_db():
         Recorder.called = True
-	
+
     monkeypatch.setattr('gitTogether.db.init_db', fake_init_db)
-    result = runner.invoke(args['init-db'])
+    result = runner.invoke(args=['init-db'])
     assert 'Initialized' in result.output
     assert Recorder.called
--- tests/test_factory.py
+++ tests/test_factory.py
@@ -3,7 +3,7 @@ from gitTogether import create_app
 def test_config():
     assert not create_app().testing
     assert create_app({'TESTING': True}).testing
-	
+
 def test_hello(client):
-    response = client.get('/hello')
+    response = client.get('/')
     assert response.data == b'Hello, World!'
