da211c21e8b0ab5c8e9448fa0142dd9491f1dfad
Hunter Parks <hunter.thomas.parks@gmail.com>
2019-03-04 02:32:48 +0900

    add 'about' endpoint and jsonify functionality

--- gitTogether/home/routes.py
+++ gitTogether/home/routes.py
@@ -1,8 +1,15 @@
-from flask import request, Blueprint
+from flask import request, Blueprint, jsonify
 
 home = Blueprint('home', __name__)
 
 @home.route("/")
 @home.route("/home")
 def index():
-    return 'Hello, World!'
+    test = {
+        'test': 'Hello, World',
+    }
+    return jsonify(test)
+
+@home.route("/about")
+def about():
+    return jsonify({'page': 'About us!'})
